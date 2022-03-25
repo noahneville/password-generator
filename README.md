fba93646b61259266171da5ba7237684e10f32fe
Noah Brunner <noahneville@gmail.com>
2022-03-24 23:28:24 -0400

    completed script.js file to function properly, created readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# password-generator
-Given a set of user-specified parameters, randomly generates a strong password
+# Password Generator
+
+For this project, I used JavaScript to build a simple password generator that, given a set of user-specified parameters, outputs a string of randomly selected characters. 
+
+To achieve this, I used the prompt and confirm methods to gather a series of criteria from the user about the length of password they want, as well as which types of characters to include. 
+
+I then created an array using the concatenation method from arrays of each character type. Then I created a 'for loop' on that array for the desired password length that used the Math.floor and Math.random methods to come up with a string of random characters, which was the final output of the function. 
+
+I found this project to be pretty straight forward once I sat down and broke down the problem into pseudocode. At first, I thought to write 'if/else' statements for each possible combination of criteria, but then realized I could be much more efficient by using boolean variables defined by my Confirm methods to concatenate my array without the need for a massive list of if/else statements. By sitting down to pseudocode and figure out exactly how I wanted to attack the problem before writing any code, I saved myself a ton of time and the only things I really had to fix were typos and several instances where I used the wrong variables. In fact, the biggest take away from this project name for me is to not have so many variables with nearly identical names, to help avoid confusion.
+
+## Acknowledgements 
+
+Thanks to the members of my bootcamp study group, who help by letting me bounce ideas off them and also help me get better at debugging as I help them find that one } they forgot on line 132. 
+    
+    - Seamona Stewart, who hosts our Zoom calls. Truly our Atlus, holding the whole study group on her back.
+    - Brian Samuels
+    - James Edwards
+    - Eric Hurst
+    - James Edwards
+    - Jason Couch
+    - Anthony Chang
+
+Also thanks to GitHub user kwiniarski97, who posted [this comment](https://gist.github.com/bendc/1e6af8f2d8027f2965da?permalink_comment_id=2846400#gistcomment-2846400) with arrays of the entire alphabet, in both upper and lower case so I didn't have to type them by hand. 
+
+I got my special characters list from [OWASP](https://owasp.org/www-community/password-special-characters), but had to type out all the quotation marks and commas by hand. 
+
--- script.js
+++ script.js
@@ -7,8 +7,60 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// variables for each of the four types of characters usable for password
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// variables to contain user inputs for password length and each of the four character types
+var passwordLength;
+var includeLowercase;
+var includeUppercase;
+var includeNumbers;
+var includeSpecial;
+// variable to contain the selected arrays
+var passwordParameters = [];
+// this variable stores the final result. Spent a good 40 minutes trying to figure out why my password always printed 'undefined' before realizing it's because the += function doesn't overwrite the undefined state and just adds to the end of it
+var finalResult = "";
+
+function generatePassword(){
+  // prompts user to choose a password length and checks to make sure it is within the allowed range
+  passwordLength = window.prompt("Please select a password length between 8 and 128 characters.");
+  if ( passwordLength < 8 || passwordLength > 128) {
+    passwordLength = window.prompt("The value you selected is not within the acceptable range. Please choose a number between 8 and 128");
+  };
+  
+  // use confirm method to determine which character types will be used
+  includeLowercase = window.confirm("Would you like to include lowercase letters in this password?");
+  includeUppercase = window.confirm("Would you like to include uppercase letters in this password?");
+  includeNumbers = window.confirm("Would you like to include numbers in this password?");
+  includeSpecial= window.confirm("Would you like to include special characters in this password?");
+  
+  // adds user-selected criteria to one large array
+  if(includeLowercase === true) {
+    passwordParameters = passwordParameters.concat(lowerCase);
+  }
+
+  if(includeUppercase === true) {
+    passwordParameters = passwordParameters.concat(upperCase);
+  }
+
+  if(includeNumbers === true) {
+    passwordParameters = passwordParameters.concat(numbers);
+  }
+
+  if(includeSpecial === true) {
+    passwordParameters = passwordParameters.concat(specialCharacters);
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    finalResult += passwordParameters[Math.floor(Math.random() * passwordParameters.length)];
+  }
+
+  return finalResult;
+};
